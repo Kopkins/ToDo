43ac39056a3c92f34b784ddff86ce452a4c361b8
Kyle Hopkins <kylehopkins1215@gmail.com>
2014-08-07 13:02:38 -0400

    Added .desktop and .install files

--- todo.py
+++ todo.py
@@ -11,7 +11,7 @@ class ToDoList(Gtk.Window):
         self.numItems = 0
         self.lastItem = None
         self.listItems = list()
-        self.filePath = os.path.join(os.environ['HOME'], '.ToDoList') 
+        self.filePath = os.path.join(os.environ['HOME'], '.config/.ToDoList') 
         self.grid = Gtk.Grid()
         self.grid.set_row_spacing(10)
         self.grid.set_column_spacing(15)
